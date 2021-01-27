99d554615b6553eb829ae9c34317525e1456642e
unknown <nellysherova@mail.ru>
2021-01-27 23:14:39 +0300

    Начало работы

---
+++ ClassCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
